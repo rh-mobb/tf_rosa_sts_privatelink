0f1ebd20f104f77fd86058c7039e9fc75c42cb38
MHS <msarvest@redhat.com>
2022-06-22 12:27:03 -0700

    add egress proxy instance

---
+++ proxy.tf
@@ -0,0 +1,33 @@
+resource "aws_network_interface" "proxy_interface" {
+    subnet_id = "${var.subnet_id}"
+
+    # Important to disable this check to allow traffic not addressed to the
+    # proxy to be received
+    source_dest_check = false
+}
+
+resource "aws_instance" "egress_proxy" {
+    ami =  var.bastion_ami
+    instance_type = var.bastion_instance_type
+    key_name = aws_key_pair.bastion_key_pair.key_name
+
+    user_data = "${file("egress_proxy_user_data.sh")}"
+
+    network_interface {
+        network_interface_id = "${aws_network_interface.proxy_interface.id}"
+        device_index = 0
+    }
+
+    tags {
+        Name = "egress_proxy"
+    }
+}
+
+# Outputs
+output "proxy_public_ip" {
+    value = "${aws_instance.egress_proxy.public_ip}"
+}
+
+output "proxy_network_interface_id" {
+    value = "${aws_network_interface.egress_proxy.id}"
+}
