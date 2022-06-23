cdd4699fe50b5d3f377aac7e22801e844c27ef5f
MHS <msarvest@redhat.com>
2022-06-22 19:22:01 -0700

    add key pair to instance and add security group

--- proxy.tf
+++ proxy.tf
@@ -1,4 +1,13 @@
-resource "aws_network_interface" "proxy_interface" {
+resource "aws_key_pair" "egress-proxy-key-pair" {
+  key_name   = "${local.name}-egress-proxy"
+  public_key = file(var.bastion_key_loc)
+  tags = {
+    Name = "${local.name}-egress-proxy-key-pair"
+  }
+ }
+
+
+resource "aws_network_interface" "egress_proxy_interface" {
     subnet_id = aws_subnet.egress_vpc_pub_subnet.id
 
     # Important to disable this check to allow traffic not addressed to the
@@ -12,12 +21,12 @@ resource "aws_network_interface" "proxy_interface" {
 resource "aws_instance" "egress_proxy" {
     ami =  var.bastion_ami
     instance_type = var.bastion_instance_type
-    key_name = aws_key_pair.bastion_key_pair.key_name
+    key_name = aws_key_pair.egress-proxy-key-pair.key_name
 
     user_data = "${file("egress_proxy_user_data.sh")}"
 
     network_interface {
-        network_interface_id = "${aws_network_interface.proxy_interface.id}"
+        network_interface_id = "${aws_network_interface.egress_proxy_interface.id}"
         device_index = 0
     }
 
@@ -26,11 +35,39 @@ resource "aws_instance" "egress_proxy" {
     }
 }
 
+# create security group for egress proxy
+resource "aws_security_group" "egress-proxy" {
+
+  description = "egress proxy"
+  name = "${local.name}-egress-proxy"
+  vpc_id = aws_vpc.egress_vpc.id
+  # Created an inbound rule for egress proxy SSH
+  ingress {
+    description = "input for egress proxy"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+  }
+
+  egress {
+    description = "output from egress proxy"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "${local.name}-egress-proxy"
+  }
+
+}
+
+
 # Outputs
 output "proxy_public_ip" {
     value = "${aws_instance.egress_proxy.public_ip}"
 }
 
 output "proxy_network_interface_id" {
-    value = "${aws_network_interface.proxy_interface.id}"
+    value = "${aws_network_interface.egress_proxy_interface.id}"
 }
